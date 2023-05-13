0def5001ded77ef9fcc121d162aef13911b6effd
snzvss <131318379+snzvss@users.noreply.github.com>
2023-05-13 03:48:00 -0500

    Update colocarPuntos.js

--- colocarPuntos.js
+++ colocarPuntos.js
@@ -1,8 +1,4 @@
 function colocarPuntos(datos) {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: { lat: 10.858500910998263, lng: -74.77435608030717 }
-    });
 
     datos.forEach(function (dato) {
         var marker = new google.maps.Marker({
